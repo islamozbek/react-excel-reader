a41058cfc01255e3720c12e4f26b1170bbdba20b
İslam Özbek <web.islamozbek@gmail.com>
2018-02-08 21:12:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Excel Reader with React</h1>
+
+<h3>Install</h3>
+npm install
+<br />
+<h3>Run</h3>
+npm start
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SheetJSApp from './sheetjs.jsx'
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <SheetJSApp />
+    );
+  }
+}
+
+export default App;
